76827740b7f403d6bd6a1196e889cb885878032a
Linh Nguyen <linhngchi@live.com>
2018-05-09 16:57:42 +0700

    Fix method signature

--- index.d.ts
+++ index.d.ts
@@ -1,74 +1,61 @@
-declare module 'react-native-twilio-programmable-voice' {
+declare module "react-native-twilio-programmable-voice" {
   export interface TwilioParam {
-    call_sid?: string
-    call_state?:
-      | 'PENDING'
-      | 'CONNECTED'
-      | 'ACCEPTED'
-      | 'CONNECTING'
-      | 'DISCONNECTED'
-      | 'CANCELLED'
-    error?: string
-    call_from?: string
-    call_to?: string
-    from?: string
-    to?: string
+    call_sid?: string;
+    call_state?: "PENDING" | "CONNECTED" | "ACCEPTED" | "CONNECTING" | "DISCONNECTED" | "CANCELLED";
+    error?: string;
+    call_from?: string;
+    call_to?: string;
+    from?: string;
+    to?: string;
   }
 
-  /*async*/ function initWithToken(
-    token: string
-  ): Promise<{ initialized: boolean }>
+  /*async*/ function initWithToken(token: string): Promise<{ initialized: boolean }>;
 
-  function initWithTokenUrl(url: string)
-  function connect(params: { To: string })
-  function disconnect()
-  function setMuted(isMuted: boolean)
-  function setSpeakerPhone(value: number)
-  function sendDigits(digits: number)
-  function requestPermissions(senderId: string)
-  function getActiveCall(): Promise<TwilioParam>
+  function initWithTokenUrl(url: string);
+  function connect(params: { To: string });
+  function disconnect();
+  function setMuted(isMuted: boolean);
+  function setSpeakerPhone(value: boolean);
+  function sendDigits(digits: number);
+  function requestPermissions(senderId: string);
+  function getActiveCall(): Promise<TwilioParam>;
 
   // Android only
-  function accept()
-  function reject()
-  function ignore()
+  function accept();
+  function reject();
+  function ignore();
 
   // iOS only
-  function configureCallKit(params: {
-    appName: string
-    imageName?: string
-    ringtoneSound?: string
-  })
-  function unregister()
+  function configureCallKit(params: { appName: string; imageName?: string; ringtoneSound?: string });
+  function unregister();
 
   function addEventListener(
     type:
-      | 'deviceReady'
-      | 'deviceNotReady'
-      | 'deviceDidReceiveIncoming'
-      | 'connectionDidConnect'
-      | 'callRejected',
-    handler: any
-  )
+      | "deviceReady"
+      | "deviceNotReady"
+      | "connectionDidConnect"
+      | "connectionDidDisconnect"
+      // iOS
+      | "callRejected"
+      // Android
+      | "deviceDidReceiveIncoming"
+      | "proximity"
+      | "wiredHeadset",
+    handler: (data?: any) => void
+  );
 
   function removeEventLisner(
     type:
-      | 'deviceReady'
-      | 'deviceNotReady'
-      | 'deviceDidReceiveIncoming'
-      | 'connectionDidConnect'
-      | 'callRejected',
-    handler: any
-  )
-
-  function addDeviceReadyListener(handler: any)
-  function removeDeviceReadyListener(handler: any)
-  function addDeviceNotReadyListener(handler: any)
-  function removeDeviceNotReadyListener(handler: any)
-  function addReceiveIncomingListener(handler: any)
-  function removeReceiveIncomingListener(handler: any)
-  function addConnectionConnectListener(handler: any)
-  function removeConnectionConnectListener(handler: any)
-  function addCallRejectedListener(handler: any)
-  function removeCallRejectedListener(handler: any)
+      | "deviceReady"
+      | "deviceNotReady"
+      | "connectionDidConnect"
+      | "connectionDidDisconnect"
+      // iOS
+      | "callRejected"
+      // Android
+      | "deviceDidReceiveIncoming"
+      | "proximity"
+      | "wiredHeadset",
+    handler: (data?: any) => void
+  );
 }
--- index.js
+++ index.js
@@ -1,141 +1,112 @@
-import {
-    NativeModules,
-    NativeEventEmitter,
-    Platform,
-} from 'react-native'
+import { NativeModules, NativeEventEmitter, Platform } from "react-native";
 
-const ANDROID = 'android'
-const IOS = 'ios'
+const ANDROID = "android";
+const IOS = "ios";
 
-const TwilioVoice = NativeModules.RNTwilioVoice
+const TwilioVoice = NativeModules.RNTwilioVoice;
 
-const NativeAppEventEmitter = new NativeEventEmitter(TwilioVoice)
+const NativeAppEventEmitter = new NativeEventEmitter(TwilioVoice);
 
 const _eventHandlers = {
-    deviceReady: new Map(),
-    deviceNotReady: new Map(),
-    deviceDidReceiveIncoming: new Map(),
-    connectionDidConnect: new Map(),
-    connectionDidDisconnect: new Map(),
-    //iOS specific
-    callRejected: new Map(),
-}
+  deviceReady: new Map(),
+  deviceNotReady: new Map(),
+  deviceDidReceiveIncoming: new Map(),
+  connectionDidConnect: new Map(),
+  connectionDidDisconnect: new Map(),
+  //iOS specific
+  callRejected: new Map()
+};
 
 const Twilio = {
-    // initialize the library with Twilio access token
-    // return {initialized: true} when the initialization started
-    // Listen to deviceReady and deviceNotReady events to see whether
-    // the initialization succeeded
-    async initWithToken(token) {
-        const result = await TwilioVoice.initWithAccessToken(token)
-        // native react promise present only for Android
-        // iOS initWithAccessToken doesn't return
-        if (Platform.OS === IOS) {
-            return {
-                initialized: true,
-            }
-        }
-        return result
-    },
-    initWithTokenUrl(url) {
-        if (Platform.OS === IOS) {
-            TwilioVoice.initWithAccessTokenUrl(url)
-        }
-    },
-    connect(params = {}) {
-        TwilioVoice.connect(params)
-    },
-    disconnect() {
-        TwilioVoice.disconnect()
-    },
-    accept() {
-        if (Platform.OS === IOS) {
-            return
-        }
-        TwilioVoice.accept()
-    },
-    reject() {
-        if (Platform.OS === IOS) {
-            return
-        }
-        TwilioVoice.reject()
-    },
-    ignore() {
-        if (Platform.OS === IOS) {
-            return
-        }
-        TwilioVoice.ignore()
-    },
-    setMuted(isMuted) {
-        TwilioVoice.setMuted(isMuted)
-    },
-    setSpeakerPhone(value) {
-        TwilioVoice.setSpeakerPhone(value)
-    },
-    sendDigits(digits) {
-        TwilioVoice.sendDigits(digits)
-    },
-    requestPermissions(senderId) {
-        if (Platform.OS === ANDROID) {
-            TwilioVoice.requestPermissions(senderId)
-        }
-    },
-    getActiveCall() {
-        return TwilioVoice.getActiveCall()
-    },
-    configureCallKit(params = {}) {
-        if (Platform.OS === IOS) {
-            TwilioVoice.configureCallKit(params)
-        }
-    },
-    unregister() {
-        if (Platform.OS === IOS) {
-            TwilioVoice.unregister()
-        }
-    },
-    addDeviceReadyListener(handler) {
-        this.addEventListener('deviceReady', handler)
-    },
-    removeDeviceReadyListener(handler) {
-        this.removeEventListener('deviceReady', handler)
-    },
-    addDeviceNotReadyListener(handler) {
-        this.addEventListener('deviceNotReady', handler)
-    },
-    removeDeviceNotReadyListener(handler) {
-        this.removeEventListener('deviceNotReady', handler)
-    },
-    addReceiveIncomingListener(handler) {
-        this.addEventListener('deviceDidReceiveIncoming', handler)
-    },
-    removeReceiveIncomingListener(handler) {
-        this.removeEventListener('deviceDidReceiveIncoming', handler)
-    },
-    addConnectionConnectListener(handler) {
-        this.addEventListener('connectionDidConnect', handler)
-    },
-    removeConnectionConnectListener(handler) {
-        this.removeEventListener('connectionDidConnect', handler)
-    },
-    addCallRejectedListener(handler) {
-        this.addEventListener('callRejected', handler)
-    },
-    removeCallRejectedListener(handler) {
-        this.removeEventListener('callRejected', handler)
-    },
-    addEventListener(type, handler) {
-        if (_eventHandlers[type].has(handler)) {
-            return
-        }
-        _eventHandlers[type].set(handler, NativeAppEventEmitter.addListener(type, rtn => { handler(rtn) }))
-    },
-    removeEventListener(type, handler) {
-        if (!_eventHandlers[type].has(handler)) {
-            return
-        }
-        _eventHandlers[type].get(handler).remove()
-        _eventHandlers[type].delete(handler)
+  // initialize the library with Twilio access token
+  // return {initialized: true} when the initialization started
+  // Listen to deviceReady and deviceNotReady events to see whether
+  // the initialization succeeded
+  async initWithToken(token) {
+    const result = await TwilioVoice.initWithAccessToken(token);
+    // native react promise present only for Android
+    // iOS initWithAccessToken doesn't return
+    if (Platform.OS === IOS) {
+      return {
+        initialized: true
+      };
     }
-}
+    return result;
+  },
+  initWithTokenUrl(url) {
+    if (Platform.OS === IOS) {
+      TwilioVoice.initWithAccessTokenUrl(url);
+    }
+  },
+  connect(params = {}) {
+    TwilioVoice.connect(params);
+  },
+  disconnect() {
+    TwilioVoice.disconnect();
+  },
+  accept() {
+    if (Platform.OS === IOS) {
+      return;
+    }
+    TwilioVoice.accept();
+  },
+  reject() {
+    if (Platform.OS === IOS) {
+      return;
+    }
+    TwilioVoice.reject();
+  },
+  ignore() {
+    if (Platform.OS === IOS) {
+      return;
+    }
+    TwilioVoice.ignore();
+  },
+  setMuted(isMuted) {
+    TwilioVoice.setMuted(isMuted);
+  },
+  setSpeakerPhone(value) {
+    TwilioVoice.setSpeakerPhone(value);
+  },
+  sendDigits(digits) {
+    TwilioVoice.sendDigits(digits);
+  },
+  requestPermissions(senderId) {
+    if (Platform.OS === ANDROID) {
+      TwilioVoice.requestPermissions(senderId);
+    }
+  },
+  getActiveCall() {
+    return TwilioVoice.getActiveCall();
+  },
+  configureCallKit(params = {}) {
+    if (Platform.OS === IOS) {
+      TwilioVoice.configureCallKit(params);
+    }
+  },
+  unregister() {
+    if (Platform.OS === IOS) {
+      TwilioVoice.unregister();
+    }
+  },
+  addEventListener(type, handler) {
+    if (_eventHandlers[type].has(handler)) {
+      return;
+    }
+    _eventHandlers[type].set(
+      handler,
+      NativeAppEventEmitter.addListener(type, rtn => {
+        handler(rtn);
+      })
+    );
+  },
+  removeEventListener(type, handler) {
+    if (!_eventHandlers[type].has(handler)) {
+      return;
+    }
+    _eventHandlers[type].get(handler).remove();
+    _eventHandlers[type].delete(handler);
+  }
+};
 
-export default Twilio
+export default Twilio;
